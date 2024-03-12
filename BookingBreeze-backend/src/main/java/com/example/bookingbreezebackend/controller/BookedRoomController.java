af761e808c885e6aafbf7f3c0118246e05387e20
Dhruv <dhruvsolanki5604@gmail.com>
2024-03-12 22:35:29 +0530

    Commit

--- BookingBreeze-backend/src/main/java/com/example/bookingbreezebackend/controller/BookedRoomController.java
+++
@@ -1,4 +0,0 @@
-package com.example.bookingbreezebackend.controller;
-
-public class BookedRoomController {
-}
--- BookingBreeze-backend/src/main/java/com/example/bookingbreezebackend/service/BookedRoomServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.example.bookingbreezebackend.service;
-
-public class BookedRoomServiceImpl {
-}
